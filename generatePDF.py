813b0d8de3b1922ab2b3b2d2def6bfef90b6c6f3
Yidong <yidongmeng1992@gmail.com>
2021-05-17 21:44:00 +0200

    introduce configuration system into program

--- generatePDF.py
+++ generatePDF.py
@@ -36,6 +36,7 @@ def generate_PDF(liveinfos, img_timetable, img_producttable, h_time, h_prod):
     styles = getSampleStyleSheet()
     styles.add(ParagraphStyle(fontName='SimSun', name='Song', leading=20, fontSize=12))
     styles.add(ParagraphStyle(fontName='SimSun', name='Song_sm', leading=20, fontSize=10))
+    styles.add(ParagraphStyle(fontName='SimSun', name='Song_bg', leading=20, fontSize=14))
     story = []
     story.append(timetable_image)
     story.append(producttable_image)
@@ -48,7 +49,6 @@ def generate_PDF(liveinfos, img_timetable, img_producttable, h_time, h_prod):
 
         story.append(Paragraph('Infos :', styles['Heading5']))
         for product in live.products:
-            # print(product.name, product.codes)
             for code in product.codes:
                 story.append(Paragraph(str(code), styles['Song']))
 
@@ -57,16 +57,22 @@ def generate_PDF(liveinfos, img_timetable, img_producttable, h_time, h_prod):
             # 生成产品链接，参数产品名，产品链接
             story.append(Paragraph(link(product.name, product.link), \
                 styles['Song']))
-            story.append(Paragraph("   产品名: " + product.alias \
+            story.append(Paragraph("产品名: " + product.alias \
                 + ";\tID: " + product.id, styles['Song_sm']))
+            if product.codes:
+                story.append(Paragraph("Codes: ", styles['Bullet']))
+                for code in product.codes:
+                    story.append(Paragraph(code, styles['Song_sm']))
             # 产品ID
             if i < len(live.products) -1 :
                 ids = ids + product.id + ","
             else :
                 ids = ids + product.id
             i += 1
+            story.append(Paragraph('-'*25, styles['Heading2']))
 
         story.append(Paragraph('Products ID :', styles['Heading5']))
         story.append(Paragraph(ids, styles['Italic']))
+        story.append(Paragraph('='*40, styles['Heading1']))
 
     doc.build(story)
--- generate_daily_live_file.py
+++ generate_daily_live_file.py
@@ -1,53 +1,72 @@
 import pandas as pd
-import os, math
-from datetime import datetime
+import os, json
+from datetime import datetime, timedelta
 from LiveInfo import LiveInfo
 from Product import Product
 import generatePDF
 
-__default_sourcefile__ = 'C:\\Users\\yidon\\OneDrive\\桌面\\05月排期细表.xlsx'
-__product_file__ = 'C:\\Users\\yidon\\OneDrive\\桌面\\5月UGC产品总表.xlsx'
-__image_timetable__ = 'C:\\Users\\yidon\\OneDrive\\桌面\\t.png'
-__image_producttable__ = 'C:\\Users\\yidon\\OneDrive\\桌面\\p.png'
-__normal_live_types__ = ["UGC","IP-UGC","运动","华为首发"]
+# Load configurations
+def loadConfig(config_path):
+    f = open(config_path,'r', encoding='UTF-8')
+    config_data = json.load(f)
+    return config_data
+
+workpath = os.path.abspath(os.path.join(os.getcwd(), ""))
+config = loadConfig(workpath+'/config.json')
 
 def read_timetable(source_file, date):
     source_table = pd.read_excel(source_file)
-    second_table = pd.read_excel(source_file, 1)
-    product_table = pd.read_excel(__product_file__)
-    product_table['主播'] = product_table['主播']\
+    account_table = pd.read_excel(source_file, 1)
+    product_table = pd.read_excel(config['product_file'])
+
+    prod_influencer_col = config["producttable_influencer_column_title"]
+    prod_link_col = config["producttable_link_column_num"]
+    prod_id_col = config["producttable_id_column_num"]
+    prod_name_col = config["producttable_name_column_num"]
+    prod_alias_col = config["producttable_alias_column_num"]
+    prod_code_col = config["producttable_code_column_num"]
+
+    src_influencer_col = config["sourcetable_influencer_column_title"]
+    src_date_col = config["sourcetable_date_column_title"]
+    src_livetype_col = config["sourcetable_livetype_column_title"]
+
+    account_influencer_col = config["accounttable_influencer_column_title"]
+    account_accountmail_col = config["accounttable_account_column_title"]
+
+    product_table[prod_influencer_col] = product_table[prod_influencer_col]\
         .fillna(method='pad')
-    product_table['主播'] = product_table['主播'].str.strip()
-    datas = source_table.loc[source_table['日期'] == date]
-    influencers = datas['主播']
+    product_table[prod_influencer_col] = product_table[prod_influencer_col].str.strip()
+    datas = source_table.loc[source_table[src_date_col] == date]
+    influencers = datas[src_influencer_col]
     liveinfos = []
     for influencer in influencers:
         print(influencer)
         products = []
-        liveline = datas.loc[datas['主播'] == influencer.strip()]
-        livetypes = liveline['类型'].to_numpy()
+        liveline = datas.loc[datas[src_influencer_col] == influencer.strip()]
+        livetypes = liveline[src_livetype_col].to_numpy()
         for livetype in livetypes:
-            if livetype in __normal_live_types__:
-                account_line = second_table.loc[
-                    second_table['主播'] == influencer.strip()]
-                account = account_line['账号'].to_numpy()[0]
+            if livetype in config['single_live_types']:
+                account_line = account_table.loc[
+                    account_table[account_influencer_col] == influencer.strip()]
+                account = account_line[account_accountmail_col].to_numpy()[0]
                 products_list = product_table.loc[
-                    product_table['主播'] == influencer.strip()
+                    product_table[prod_influencer_col] == influencer.strip()
                     ].to_numpy()
-                for prod in products_list:
-                    # 如果产品链接非空
-                    if isinstance(prod[2], str):
-                        # 新Product对象，参数产品ID
-                        product = Product(str(int(prod[3])).strip())
-                        product.codes = []
-                        product.name = prod[4]
-                        product.alias = prod[5]
-                        product.link = prod[2].strip()
-                        # 如果产品Code非空
-                        if isinstance(prod[6], str):
-                            # 添加产品Code
-                            product.codes.append(prod[6])
-                        products.append(product)
+                if livetype in config['normal_live_types']:
+                    for prod in products_list:
+                        # 如果产品链接非空
+                        if isinstance(prod[prod_link_col], str):
+                            # 新Product对象，参数产品ID
+                            product = Product(str(int(prod[prod_id_col])).strip())
+                            product.codes = []
+                            product.name = prod[prod_name_col]
+                            product.alias = prod[prod_alias_col]
+                            product.link = prod[prod_link_col].strip()
+                            # 如果产品Code非空
+                            if isinstance(prod[prod_code_col], str):
+                                # 添加产品Code
+                                product.codes.append(prod[prod_code_col])
+                            products.append(product)
                 liveinfo = LiveInfo(influencer, account, date, products)
                 liveinfos.append(liveinfo)
 
@@ -55,18 +74,22 @@ def read_timetable(source_file, date):
 
 
 if __name__ == '__main__':
-    workpath = os.path.abspath(os.path.join(os.getcwd(), ""))
     while True:
         source_file = input("输入要导入的源排期表路径：")
         if not source_file : 
-            source_file = __default_sourcefile__
+            source_file = config['default_sourcefile']
             print(source_file)
         if os.path.exists(source_file):
             break
     while True:
-        date_text = input("输入要生成的文件对应日期(yyyymmdd)：")
+        today = datetime.today()
+        tomorrow = today + timedelta(1)
+        date_text = input("输入要生成的文件对应日期(yyyymmdd, "\
+            "'+' for tomorrow)：")
         if not date_text:
-            date_text = datetime.today().strftime('%Y%m%d')
+            date_text = today.strftime('%Y%m%d')
+        if date_text == '+':
+            date_text = tomorrow.strftime('%Y%m%d')
 
         try:
             date = datetime.strptime(date_text, '%Y%m%d')
@@ -75,9 +98,10 @@ if __name__ == '__main__':
         except Exception as e:
             pass
     while True:
-        h_time_string = input("输入时间表高度（default 150）：")
+        h_time_string = input("输入时间表高度（default " \
+            + str(config['timetable_height']) + "）：")
         if not h_time_string:
-            h_time = 150
+            h_time = config['timetable_height']
         else:
             h_time = int(h_time_string)
 
@@ -85,9 +109,10 @@ if __name__ == '__main__':
             print('时间表高度: ', h_time)
             break
     while True:
-        h_prod_string = input("输入产品表高度（default 450）：")
+        h_prod_string = input("输入产品表高度（default " \
+            + str(config['producttable_height']) + "）：")
         if not h_prod_string:
-            h_prod = 450
+            h_prod = config['producttable_height']
         else:
             h_prod = int(h_prod_string)
 
@@ -96,5 +121,5 @@ if __name__ == '__main__':
             break
 
     live_infos = read_timetable(source_file, date)
-    generatePDF.generate_PDF(live_infos, __image_timetable__, \
-        __image_producttable__, h_time, h_prod)
+    generatePDF.generate_PDF(live_infos, config['image_timetable'], \
+        config['image_producttable'], h_time, h_prod)
